ccc195da4d98ff70bfad6c19891c3379df15d4e4
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2022-11-06 12:05:13 -0600

    Added support for environment variables

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+export const environments = {
+  HOST_MYSQL: process.env.HOST_MYSQL,
+  USER_MYSQL: process.env.USER_MYSQL,
+  PASSWORD_MYSQL: process.env.PASSWORD_MYSQL,
+  PORT_MYSQL: process.env.PORT_MYSQL,
+  DATABASE_MYSQL: process.env.DATABASE_MYSQL
+}
